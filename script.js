ab3acd68c2a050dc2e61a2e4461d4b09fc3479e6
mlaane <goobric@gmail.com>
2023-12-30 17:36:30 +0000

    added variables

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const currentDateParagraph = document.getElementById('current-date');
+const dateOptionsSelectElement = document.getElementById('date-options');
+// const dateOptions = {
+//   weekday: 'long',
+//   year: 'numeric',
+//   month: 'long',
+//   day: 'numeric',
+// };
+const date = new Date();
